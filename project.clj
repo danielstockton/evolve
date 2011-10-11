a09a3dbfb62ee10550d41d1020aa1a76dc2e92fb
Daniel Stockton <me@danielstockton.com>
2011-10-11 22:37:23 +0300

    removing swp files from history

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "String Evolution"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[org.clojars.scott/lein-nailgun "1.1.0"]]
   :main evolve.core)
