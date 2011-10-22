62593709c544faf67748321d86746ab2c0471558
Daniel Stockton <me@danielstockton.com>
2011-10-23 01:19:11 +0300

    needs improving

--- src/evolve/core.clj
+++ src/evolve/core.clj
@@ -27,31 +27,45 @@
         (contstr/codepoints source) 
         (contstr/codepoints target)))))
 
-(defn pop-fitness
+(defn popfitness
   "maps a fitness to each member of the population"
   [population fitness]
-  (map 
-    #(fitness %)
-    population))
+  (into {} (map #(vector % (fitness %)) population)))
 
-(defn mutate 
-  [source]
+
+;;; currently selects unfit parents
+(defn selection
+  "selects two parents with a higher probability of selecting fit parents"
+  [popfitness]
+  (let [total (apply + (vals popfitness))] 
+    (take 2 
+      (shuffle 
+        (for [[k v] popfitness itm (repeat (* 1000000 (/ 1 (inc v))) k)] itm)))))
+
+(defn mutate [source]
   (let [i (rand-int (count source))] 
     (str 
       (subs source 0 i) (char (+ (rand-nth [-1 1]) (first (contstr/codepoints (str (nth source i)))))) (subs source (inc i)))))
 
+(defn breed 
+  "breeds two parents together to create two offspring"
+  [parent1 parent2]
+  (let [l1 (count parent1) l2 (count parent2)] 
+    (mutate 
+      (apply str 
+        (concat 
+          (subs parent1 0 (/ l1 2)) 
+          (subs parent2 (/ l2 2) l2))))))
+
 (defn evolve
-  [source fitness]
-  (if 
-    (= (fitness source) 0) 
-    (str source) 
-    (first 
-      (filter #(<= (fitness %) (fitness source)) 
-        (repeatedly #(mutate source))))))
+  [population target]
+  (repeatedly (count population) #(breed (first (selection (popfitness population (fitness-fn target)))) (last (selection (popfitness population (fitness-fn target)))))))
+
 
 (defn start
-  [generations source target]
-  (nth (map-indexed (fn [idx itm] (doto (str idx " " ((fitness-fn target) itm) " " itm) println)) (iterate #(evolve % (fitness-fn target)) source)) generations))
+  [generations popsize target] 
+  (let [populate (population popsize (count target))] 
+    (nth (iterate #(evolve % target) populate) generations)))
 
-(defn -main [generations source target]
-  (start (new Integer generations) source target))
+(defn -main [generations popsize target]
+  (start (new Integer generations) (new Integer popsize) target))
